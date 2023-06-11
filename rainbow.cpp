9ddea1fed5e3e7d2e912614d2f31b60cef597e62
Zoltán Balázs <git@zoltanbalazs.com>
2023-06-11 20:27:07 +0200

    Switched over to double values instead of floats - Fixed a bug of not using epsilons to check FP equality

--- rainbow.cpp
+++ rainbow.cpp
@@ -3,19 +3,19 @@
 #include <iostream>
 
 typedef struct {
-  float x;
-  float y;
-  float z;
-} float3;
+  double x;
+  double y;
+  double z;
+} double3;
 
 typedef struct {
-  float3 coord;
-  float r;
+  double3 coord;
+  double r;
 } sphere_t;
 
 typedef struct {
-  float3 coord;
-  float3 dir;
+  double3 coord;
+  double3 dir;
   double wavelength;
 } light_t;
 
@@ -25,27 +25,32 @@ typedef struct {
 } intersection_t;
 
 /* Calculates the dot product of two 3D vectors */
-float dot(float3 lhs, float3 rhs) {
+double dot(double3 lhs, double3 rhs) {
   return lhs.x * rhs.x + lhs.y * rhs.y + lhs.z * rhs.z;
 }
 
 /* Multiply a 3D vector by a scalar */
-float3 operator*(float3 vec, float scalar) {
+double3 operator*(double3 vec, double scalar) {
   return {vec.x * scalar, vec.y * scalar, vec.z * scalar};
 }
 
-float3 operator*(float scalar, float3 vec) {
+double3 operator*(double scalar, double3 vec) {
   return {vec.x * scalar, vec.y * scalar, vec.z * scalar};
 }
 
 /* Subtract two 3D vectors */
-float3 operator-(float3 lhs, float3 rhs) {
+double3 operator-(double3 lhs, double3 rhs) {
   return {lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z};
 }
 
+/* Add two 3D vectors */
+double3 operator+(double3 lhs, double3 rhs) {
+  return {lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z};
+}
+
 /* Normalize a 3D vector */
-float3 normalize(float3 v) {
-  float magnitude =
+double3 normalize(double3 v) {
+  double magnitude =
       std::sqrt(std::pow(v.x, 2) + std::pow(v.y, 2) + std::pow(v.z, 2));
   return {v.x / magnitude, v.y / magnitude, v.z / magnitude};
 }
@@ -57,22 +62,24 @@ double wavelengthToRefraction(double wavelength) {
 }
 
 /* Checks if the given point is in the sphere */
-bool inSphere(sphere_t sphere, float3 coord) {
+bool inSphere(sphere_t sphere, double3 coord) {
   double epsilon = 0.0001;
 
-  return std::abs((sphere.r * sphere.r) -
-                  (std::pow((coord.x - sphere.coord.x), 2) +
-                   std::pow((coord.y - sphere.coord.y), 2) +
-                   std::pow((coord.z - sphere.coord.z), 2))) <= epsilon;
+  return (std::pow((coord.x - sphere.coord.x), 2) +
+          std::pow((coord.y - sphere.coord.y), 2) +
+          std::pow((coord.z - sphere.coord.z), 2)) <=
+         std::pow(sphere.r, 2) + epsilon;
 }
 
 /* Based on https://registry.khronos.org/OpenGL-Refpages/gl4/html/refract.xhtml
 Given a normal vector, an incident vector, and a
 wavelength, calculates the refracted vector */
-float3 refract(float3 N, float3 I, double wavelength) {
-  float eta = wavelengthToRefraction(wavelength);
-  eta = 1.0 / eta;
-  float k = 1.0 - eta * eta * (1.0 - dot(N, I) * dot(N, I));
+double3 refract(double3 N, double3 I, double wavelength, bool inWater) {
+  double eta = wavelengthToRefraction(wavelength);
+  if (!inWater) {
+    eta = 1.0 / eta;
+  }
+  double k = 1.0 - eta * eta * (1.0 - dot(N, I) * dot(N, I));
 
   if (k < 0) {
     return {0, 0, 0};
@@ -84,9 +91,7 @@ float3 refract(float3 N, float3 I, double wavelength) {
 /* Based on https://registry.khronos.org/OpenGL-Refpages/gl4/html/reflect.xhtml
 Given an incident vector and a normal vector, calculates the reflected vector,
 the normal vector must actually be normalzied for optimal results */
-float3 reflect(float3 I, float3 N) {
-  return I - 2 * dot(normalize(N), I) * normalize(N);
-}
+double3 reflect(double3 I, double3 N) { return I - 2 * dot(N, I) * N; }
 
 /* Calculates the intersections between a sphere and a radius, if there is
  * any*/
@@ -106,21 +111,21 @@ intersection_t vectorSphereIntersection(sphere_t s, light_t l) {
    (l.coord.x - c.x)^2 + (l.coord.y - c.y)^2 + (l.coord.z - c.z)^2 - r^2 = 0 */
 
   // a = l.dir.x^2 + l.dir.y^2 + l.dir.z^2
-  float a = std::pow(l.dir.x, 2) + std::pow(l.dir.y, 2) + std::pow(l.dir.z, 2);
+  double a = std::pow(l.dir.x, 2) + std::pow(l.dir.y, 2) + std::pow(l.dir.z, 2);
 
   /* b = 2 * (l.dir.x * (l.coord.x - s.coord.x) +
   l.dir.y * (l.coord.y - s.coord.y) + l.dir.z * (l.coord.z - s.coord.z)) */
-  float b = 2 * (l.dir.x * (l.coord.x - s.coord.x) +
-                 l.dir.y * (l.coord.y - s.coord.y) +
-                 l.dir.z * (l.coord.z - s.coord.z));
+  double b = 2 * (l.dir.x * (l.coord.x - s.coord.x) +
+                  l.dir.y * (l.coord.y - s.coord.y) +
+                  l.dir.z * (l.coord.z - s.coord.z));
 
   // c = (l.coord.x - c.x)^2 + (l.coord.y - c.y)^2 + (l.coord.z - c.z)^2 - r^2
-  float c = std::pow((l.coord.x - s.coord.x), 2) +
-            std::pow((l.coord.y - s.coord.y), 2) +
-            std::pow((l.coord.z - s.coord.z), 2) - std::pow(s.r, 2);
+  double c = std::pow((l.coord.x - s.coord.x), 2) +
+             std::pow((l.coord.y - s.coord.y), 2) +
+             std::pow((l.coord.z - s.coord.z), 2) - std::pow(s.r, 2);
 
   // discriminant = b^2 - 4 * a * c
-  float d = std::pow(b, 2) - 4 * a * c;
+  double d = std::pow(b, 2) - 4 * a * c;
 
   // If the discriminant is negative, there is no solution
   intersection_t i;
@@ -129,18 +134,19 @@ intersection_t vectorSphereIntersection(sphere_t s, light_t l) {
     return i;
   }
 
-  float t1 = (-1 * b + std::sqrt(d)) / (2 * a);
-  float t2 = (-1 * b - std::sqrt(d)) / (2 * a);
+  double t1 = (-1 * b + std::sqrt(d)) / (2 * a);
+  double t2 = (-1 * b - std::sqrt(d)) / (2 * a);
 
-  float t = 0;
+  double epsilon = 0.0001;
+  double t = 0;
 
   // If t1 is positive, is smaller than t2 or t2 is negative, we use t1
   // If t2 is positive, is smaller than t1 or t1 is negative, we use t2
   // If both are negative, there is no intersection
-  if (0 < t1 && (t1 < t2 || t2 <= 0)) {
+  if (0 < t1 && (t1 < t2 || std::abs(t2) <= epsilon)) {
     i.intersects = true;
     t = t1;
-  } else if (0 < t2 && (t2 < t1 || t1 <= 0)) {
+  } else if (0 < t2 && (t2 < t1 || std::abs(t1) <= epsilon)) {
     i.intersects = true;
     t = t2;
   } else {
@@ -160,28 +166,28 @@ intersection_t vectorSphereIntersection(sphere_t s, light_t l) {
 }
 
 /* Calculates the normal vector for a sphere and intersection point */
-float3 calculateNormalVector(sphere_t s, float3 i) {
+double3 calculateNormalVector(sphere_t s, double3 i) {
   /* Given a sphere and a point on the sphere's surface, calculate the
    * vector from the sphere's center to the intersection point */
-  float3 vector = {i.x - s.coord.x, i.y - s.coord.y, i.z - s.coord.z};
+  double3 vector = {i.x - s.coord.x, i.y - s.coord.y, i.z - s.coord.z};
 
   /* Normalize the given vector */
   return normalize(vector);
 }
 
 /* Calculates the angle between two 3D vectors */
-float angleBetweenVectors(float3 lhs, float3 rhs) {
+double angleBetweenVectors(double3 lhs, double3 rhs) {
   /* Calculate the dot product of the vectors */
-  float dotProduct = dot(lhs, rhs);
+  double dotProduct = dot(lhs, rhs);
 
   /* Calculate the magnitudes of the vectors */
-  float magnL =
+  double magnL =
       std::sqrt(std::pow(lhs.x, 2) + std::pow(lhs.y, 2) + std::pow(lhs.z, 2));
-  float magnR =
+  double magnR =
       std::sqrt(std::pow(rhs.x, 2) + std::pow(rhs.y, 2) + std::pow(rhs.z, 2));
 
   /* Calculate the angle's cosine between the vectors */
-  float cosA = dotProduct / (magnL * magnR);
+  double cosA = dotProduct / (magnL * magnR);
 
   /* Return the radians in degrees of the angle between the vectors */
   return acos(cosA);
